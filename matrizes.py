19430d38b106b04480944993d37bcfe05e0737e3
João Pedro Martins Montera <jpmartins1013@gmail.com>
2023-08-07 10:49:46 -0400

    Add files via upload

---
+++ matrizes.py
@@ -0,0 +1,17 @@
+#MATRIZES#
+m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+i = 0
+j = 0
+
+while i <= len(m)-1:
+	print(m[i][j])
+	i += 1
+i = 0
+while i <= len(m)-1:
+	print(m[i][j+1])
+	i += 1
+i = 0
+while i <= len(m)-1:
+	print(m[i][j+2])
+	i += 1
